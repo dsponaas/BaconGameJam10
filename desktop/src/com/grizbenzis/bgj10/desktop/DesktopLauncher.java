9edf14d1ac4d50788166bb0e3cd6401edc0b88c9
sponezzis <dsponaas@gmail.com>
2016-01-22 17:31:24 -0500

    initial commit

---
+++ desktop/src/com/grizbenzis/bgj10/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.grizbenzis.bgj10.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.grizbenzis.bgj10.bgj10;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new bgj10(), config);
+	}
+}
