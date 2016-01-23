ebb827a0e70a6fb5f8bef0457160e8303e086548
sponezzis <dsponaas@gmail.com>
2016-01-23 17:00:14 -0500

    asteroid splitting bugfixes

--- core/src/com/grizbenzis/bgj10/systems/EnemyDeathSystem.java
+++ core/src/com/grizbenzis/bgj10/systems/EnemyDeathSystem.java
@@ -29,34 +29,28 @@ public class EnemyDeathSystem extends IteratingSystem {
     protected void processEntity(Entity entity, float deltaTime) {
         EnemyDataComponent enemyDataComponent = _enemyDataComponents.get(entity);
         if(!enemyDataComponent.alive) {
-            Gdx.app.log(Constants.LOG_TAG, "DEAD ENEMY");
 
             entity.remove(RenderComponent.class);
+            EntityManager.getInstance().destroyEntity(entity);
+            GameState.getInstance().incrementScore(enemyDataComponent.getPoints());
 
             if(enemyDataComponent.type == Constants.EnemyType.ASTEROID_LARGE) {
                 PositionComponent positionComponent = _positionComponents.get(entity);
                 BodyComponent bodyComponent = _bodyComponents.get(entity);
                 Vector2 startPos = new Vector2(positionComponent.x, positionComponent.y);
 
-                if(enemyDataComponent.type == Constants.EnemyType.ASTEROID_LARGE) {
-                    for(int i = 0; i < Constants.MEDIUM_ASTEROID_SPAWN_QUANTITY; ++i) {
-                        GameState.getInstance().spawnMediumAsteroid(startPos, bodyComponent.body.getLinearVelocity());
-                    }
+                for(int i = 0; i < Constants.MEDIUM_ASTEROID_SPAWN_QUANTITY; ++i) {
+                    GameState.getInstance().spawnMediumAsteroid(startPos, bodyComponent.body.getLinearVelocity());
                 }
-                else if(enemyDataComponent.type == Constants.EnemyType.ASTEROID_MEDIUM) {
-                    for(int i = 0; i < Constants.SMALL_ASTEROID_SPAWN_QUANTITY; ++i) {
-                        GameState.getInstance().spawnSmallAsteroid(startPos, bodyComponent.body.getLinearVelocity());
-                    }
+            }
+            else if(enemyDataComponent.type == Constants.EnemyType.ASTEROID_MEDIUM) {
+                PositionComponent positionComponent = _positionComponents.get(entity);
+                BodyComponent bodyComponent = _bodyComponents.get(entity);
+                Vector2 startPos = new Vector2(positionComponent.x, positionComponent.y);
+
+                for(int i = 0; i < Constants.SMALL_ASTEROID_SPAWN_QUANTITY; ++i) {
+                    GameState.getInstance().spawnSmallAsteroid(startPos, bodyComponent.body.getLinearVelocity());
                 }
-
-//                Vector2 velocity = bodyComponent.body.getLinearVelocity();
-//                Vector2 desiredVelocity = new Vector2(0f, 0f);
-//                desiredVelocity.sub(velocity).scl(bodyComponent.body.getMass());
-//                bodyComponent.body.applyLinearImpulse(desiredVelocity.x, desiredVelocity.y, bodyComponent.body.getWorldCenter().x, bodyComponent.body.getWorldCenter().y, true);
-
-                GameState.getInstance().incrementScore(enemyDataComponent.getPoints());
-
-                EntityManager.getInstance().destroyEntity(entity);
             }
         }
     }
